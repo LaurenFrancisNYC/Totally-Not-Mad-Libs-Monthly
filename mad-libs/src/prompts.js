dc72dba887f86736188afe1f9fc423cf9c1fa491
Lauren Francis Petto <laurenpetto@gmail.com>
2021-02-16 12:11:48 -0500

    test prompts created

---
+++ mad-libs/src/prompts.js
@@ -0,0 +1,52 @@
+const prompts = [
+  {
+    month: 0,
+    text: "testing ____ and ____. 0"
+  },
+  {
+    month: 1,
+    text: "testing ____ and ____. 1"
+  },
+  {
+    month: 2,
+    text: "testing ____ and ____. 2",
+  },
+  {
+    month: 3,
+    text: "testing ____ and ____. 3"
+  },
+  {
+    month: 4,
+    text: "testing ____ and ____. 4"
+  },
+  {
+    month: 5,
+    text: "testing ____ and ____. 5",
+  },
+  {
+    month: 6,
+    text: "testing ____ and ____. 6"
+  },
+  {
+    month: 7,
+    text: "testing ____ and ____. 7"
+  },
+  {
+    month: 8,
+    text: "testing ____ and ____. 8",
+  },
+  {
+    month: 9,
+    text: "testing ____ and ____. 9"
+  },
+  {
+    month: 10,
+    text: "testing ____ and ____. 10"
+  },
+  {
+    month: 11,
+    text: "testing ____ and ____. 11",
+  },
+];
+
+export default prompts;
