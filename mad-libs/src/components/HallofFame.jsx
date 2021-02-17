435a7e5184caf84b041a6563278d881b525b10d7
Lauren Francis Petto <laurenpetto@gmail.com>
2021-02-17 17:36:08 -0500

    sorted responses by most liked

--- mad-libs/src/components/HallofFame.jsx
+++ mad-libs/src/components/HallofFame.jsx
@@ -5,18 +5,26 @@ import { useState, useEffect } from "react";
 
 function HallofFame(props) {
   const [monthSelection, setMonthSelection] = useState(0)
-  
+  const [topSelection,setTopSelection] = useState('')
+
 useEffect(() => {
-  console.log(monthSelection)
+  // console.log(monthSelection)
   const responses = props.responses 
-  console.log(responses)
+  // console.log(responses)
+  const monthlyResponses = responses.filter(function(response){
+    return response.fields.month == monthSelection
+  })
 
-  // responses.filter
-
-  // const respArray = [] 
-  // responses.forEach(response =>
-  //   console.log(response.fields.month))
+  const sorted = monthlyResponses.sort((a, b) => {
+    return a.fields.likes - b.fields.likes;
+  })
   
+  console.log(sorted)
+
+
+
+
+
   }, [monthSelection]);
   
 
